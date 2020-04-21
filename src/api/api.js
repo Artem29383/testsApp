586f236f0dfcc03e5522466c4ba4af5da0ba0877
Artem <artyom.aweryanow@yandex.ru>
2020-04-21 13:43:51 +0300

    Code abbreviation

--- src/api/api.js
+++ src/api/api.js
@@ -2,32 +2,20 @@ import axios from 'axios';
 
 const baseURL = 'http://localhost:3000';
 
-export const auth = () => {
-  return axios.get(`${baseURL}/auth`);
-};
+export const auth = () => axios.get(`${baseURL}/auth`);
 
-export const getTests = () => {
-  return axios.get(`${baseURL}/tests`);
-};
+export const getTests = () => axios.get(`${baseURL}/tests`);
 
-export const getTestData = id => {
-  return axios.get(`${baseURL}/tests/${id}`);
-};
+export const getTestData = id => axios.get(`${baseURL}/tests/${id}`);
 
-export const getTestApi = id => {
-  return axios.get(`${baseURL}/tests/${id}`);
-};
+export const getTestApi = id => axios.get(`${baseURL}/tests/${id}`);
 
-export const deployingTest = data => {
-  return axios.post(`${baseURL}/tests`, data);
-};
+export const deployingTest = data => axios.post(`${baseURL}/tests`, data);
 
-export const deployingTestName = data => {
-  return axios.post(`${baseURL}/testsNames`, data);
-};
+export const deployingTestName = data =>
+  axios.post(`${baseURL}/testsNames`, data);
 
-export const updateThisTest = data => {
-  const { id, testName, entities, ids, created } = data;
+export const updateThisTest = ({ id, testName, entities, ids, created }) => {
   const questions = {
     entities,
     ids,
@@ -41,14 +29,10 @@ export const updateThisTest = data => {
   return axios.put(`${baseURL}/tests/${id}`, test);
 };
 
-export const updateFieldNameTest = data => {
-  return axios.put(`${baseURL}/testsNames/${data.id}`, data);
-};
+export const updateFieldNameTest = data =>
+  axios.put(`${baseURL}/testsNames/${data.id}`, data);
 
-export const deleteThisFilm = id => {
-  return axios.delete(`${baseURL}/tests/${id}`);
-};
+export const deleteThisFilm = id => axios.delete(`${baseURL}/tests/${id}`);
 
-export const deleteFieldNameTest = id => {
-  return axios.delete(`${baseURL}/testsNames/${id}`);
-};
+export const deleteFieldNameTest = id =>
+  axios.delete(`${baseURL}/testsNames/${id}`);
